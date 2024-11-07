30af11ccfbc29b0086a95cf79cfafabfd4a3a1ef
0age <37939117+0age@users.noreply.github.com>
2024-11-07 09:01:08 -0800

    remove all the code

--- src/types/ForcedWithdrawalStatus.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.27;
-
-enum ForcedWithdrawalStatus {
-    Disabled, // Not pending or enabled for forced withdrawal
-    Pending, // Not yet available, but initiated
-    Enabled // Available for forced withdrawal on demand
-
-}
