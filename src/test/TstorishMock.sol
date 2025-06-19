233c2b8f7f19c084bfd54bfa59c0d6a1a044f95b
0age <37939117+0age@users.noreply.github.com>
2025-06-19 11:23:55 -0400

    modify the tstorish mock a bit and test it

--- src/test/TstorishMock.sol
+++ src/test/TstorishMock.sol
@@ -4,8 +4,39 @@ pragma solidity ^0.8.27;
 import { Tstorish } from "../lib/Tstorish.sol";
 
 contract TstorishMock is Tstorish {
+    bool public mockTstoreIsActive;
 
-    function _testTload(address tloadTestContract) internal view override returns (bool ok) {
-        return tloadTestContract.code.length == 0;
+    function activateMockTstore() external {
+        mockTstoreIsActive = true;
+    }
+
+    function setMockValue() external {
+        _setTstorish(12345, 67890);
+    }
+
+    function checkMockValue() external view returns (bool mockValueSet) {
+        mockValueSet = _getTstorish(12345) == 67890;
+    }
+
+    function clearSstoreSlot() external {
+        assembly ("memory-safe") {
+            sstore(12345, 0)
+        }
+    }
+
+    function checkTstoreSlot() external view returns (bool mockValueSet) {
+        assembly ("memory-safe") {
+            mockValueSet := eq(tload(12345), 67890)
+        }
+    }
+
+    function checkSstoreSlot() external view returns (bool mockValueSet) {
+        assembly ("memory-safe") {
+            mockValueSet := eq(sload(12345), 67890)
+        }
+    }
+
+    function _testTload(address) internal view override returns (bool) {
+        return mockTstoreIsActive;
     }
 }
--- test/lib/Tstorish.t.sol
+++ test/lib/Tstorish.t.sol
@@ -15,20 +15,21 @@ contract TstorishTest is Test {
     }
 
     function test_revert_tloadTestContractDeploymentFailed() public {
-        uint8 nonce = 1;
-        address deployer = address(0x1111111111111111111111111111111111111111);
+        uint8 nonce = uint8(vm.getNonce(address(this)));
         address tstorishExpected = address(
-            uint160(
-                uint256(keccak256(abi.encodePacked(bytes1(0xd6), bytes1(0x94), address(deployer), bytes1(nonce))))));
+            uint160(uint256(keccak256(abi.encodePacked(bytes1(0xd6), bytes1(0x94), address(this), bytes1(nonce)))))
+        );
 
         address tloadTestContractExpected = address(
             uint160(
-                uint256(keccak256(abi.encodePacked(bytes1(0xd6), bytes1(0x94), address(tstorishExpected), bytes1(0x01))))));
+                uint256(
+                    keccak256(abi.encodePacked(bytes1(0xd6), bytes1(0x94), address(tstorishExpected), bytes1(0x01)))
+                )
+            )
+        );
 
         vm.etch(tloadTestContractExpected, hex"5f5ffd"); // push0 push0 revert
 
-        vm.setNonce(deployer, nonce);
-        vm.prank(deployer);
         vm.expectRevert(abi.encodeWithSelector(Tstorish.TloadTestContractDeploymentFailed.selector));
         new Tstorish();
     }
@@ -44,7 +45,11 @@ contract TstorishTest is Test {
         // Manipulate the tloadTestContract to revert so tstore is not supported
         address tloadTestContractExpected = address(
             uint160(
-                uint256(keccak256(abi.encodePacked(bytes1(0xd6), bytes1(0x94), address(tstorishContract), bytes1(0x01))))));
+                uint256(
+                    keccak256(abi.encodePacked(bytes1(0xd6), bytes1(0x94), address(tstorishContract), bytes1(0x01)))
+                )
+            )
+        );
 
         vm.etch(tloadTestContractExpected, hex"5f5ffd"); // push0 push0 revert
 
@@ -53,16 +58,45 @@ contract TstorishTest is Test {
     }
 
     function test_revert_TStoreNotSupported() public {
-        uint8 nonce = 1;
-        address deployer = address(0x1111111111111111111111111111111111111111);
-
-        vm.setNonce(deployer, nonce);
-        vm.prank(deployer);
-        
         TstorishMock tstorishContract = new TstorishMock();
 
-        // CONTINUE HERE: PROVE THAT TSTORE IS NOT SUPPORTED
-        assertEq(true, false);
+        vm.expectRevert(abi.encodeWithSelector(Tstorish.TStoreNotSupported.selector));
+        tstorishContract.__activateTstore();
+    }
 
+    function test_tstoreMockActivation() public {
+        // mock contract starts in an "tstore disabled" state
+        TstorishMock tstorishContract = new TstorishMock();
+
+        // first set the mock value via sstore
+        tstorishContract.setMockValue();
+
+        // ensure it's found correctly and set the expected storage
+        assertTrue(tstorishContract.checkMockValue());
+        assertTrue(tstorishContract.checkSstoreSlot());
+        assertFalse(tstorishContract.checkTstoreSlot());
+
+        // "enable" tstore on the mock and activate it on tstorish
+        tstorishContract.activateMockTstore();
+        tstorishContract.__activateTstore();
+
+        // sstore value is still correctly returned by tstorish this block
+        assertTrue(tstorishContract.checkMockValue());
+
+        // after 1 block it now doesn't find it anymore
+        vm.roll(block.number + 1);
+        assertFalse(tstorishContract.checkMockValue());
+
+        tstorishContract.clearSstoreSlot();
+        assertFalse(tstorishContract.checkSstoreSlot());
+        assertFalse(tstorishContract.checkTstoreSlot());
+
+        // now test setting a value using tstore
+        tstorishContract.setMockValue();
+
+        // ensure it's found correctly and set the expected storage
+        assertTrue(tstorishContract.checkMockValue());
+        assertFalse(tstorishContract.checkSstoreSlot());
+        assertTrue(tstorishContract.checkTstoreSlot());
     }
 }
